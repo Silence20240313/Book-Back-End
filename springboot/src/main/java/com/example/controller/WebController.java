77b6e473fe9fdead6f2532b99e5a3dc410cf5679
Silence20240313 <liangxiaoli378@gmail.com>
2024-12-14 20:52:40 +0900

    管理员用户相关增删改查

--- springboot/src/main/java/com/example/controller/WebController.java
+++ springboot/src/main/java/com/example/controller/WebController.java
@@ -1,18 +1,35 @@
 package com.example.controller;
 
 import com.example.common.Result;
+import com.example.entity.Employee;
+import com.example.service.EmployeeService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+    @Resource
+    private EmployeeService employeeService;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
 
-    @GetMapping("/count")
-    public Result count(){
-        return Result.success(10);
+    // 员工登录
+    @PostMapping("/login")
+    public Result login(@RequestBody Employee employee){
+        Employee dbEmployee = employeeService.login(employee);
+        return Result.success(dbEmployee);
+    }
+
+    // 员工注册
+    @PostMapping("/register")
+    public Result register(@RequestBody Employee employee){
+        employeeService.register(employee);
+        return Result.success();
     }
 }
