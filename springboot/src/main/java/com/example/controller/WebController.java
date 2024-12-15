e350c2a2ed4a04e8a1172b3012d2fc66a96d9802
Silence20240313 <liangxiaoli378@gmail.com>
2024-12-15 22:35:18 +0900

    修改密码及个人信息

--- springboot/src/main/java/com/example/controller/WebController.java
+++ springboot/src/main/java/com/example/controller/WebController.java
@@ -1,35 +1,57 @@
 package com.example.controller;
 
 import com.example.common.Result;
+import com.example.entity.Account;
 import com.example.entity.Employee;
+import com.example.exception.CustomException;
+import com.example.service.AdminService;
 import com.example.service.EmployeeService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WebController {
     @Resource
     private EmployeeService employeeService;
+    @Resource
+    private AdminService adminService;
 
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
 
-    // 员工登录
+    // 管理员/员工登录
     @PostMapping("/login")
-    public Result login(@RequestBody Employee employee){
-        Employee dbEmployee = employeeService.login(employee);
-        return Result.success(dbEmployee);
+    public Result login(@RequestBody Account account){
+        Account result = null;
+        if("ADMIN".equals(account.getRole())){  // 管理员登录
+            result = adminService.login(account);
+        }else if ("EMP".equals(account.getRole())) {
+            result = employeeService.login(account);
+        }else {
+            throw new CustomException("500","非法输入");
+        }
+        return Result.success(result);
     }
 
     // 员工注册
     @PostMapping("/register")
     public Result register(@RequestBody Employee employee){
         employeeService.register(employee);
+        return Result.success();
+    }
+
+    // 修改密码
+    @PutMapping("/updatePassword")
+    public Result updatePassword(@RequestBody Account account){
+        if("ADMIN".equals(account.getRole())){  // 管理员登录
+           adminService.updatePassword(account);
+        }else if ("EMP".equals(account.getRole())) {
+           employeeService.updatePassword(account);
+        }else {
+            throw new CustomException("500","非法输入");
+        }
         return Result.success();
     }
 }
